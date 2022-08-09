5537d40d9e29ef17064ff2ec3b91d780780741e1
SumedhBhat <sumedhbhat01@gmail.com>
2022-08-09 18:52:55 +0530

    Added multiple pages

--- client/src/Components/AdminPage/Navbar.jsx
+++ client/src/Components/AdminPage/Navbar.jsx
@@ -1,43 +1,97 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import {
   Grid,
   Typography,
   Box,
-  Container,
-  Stack,
   Button,
   Popover,
+  Table,
+  TableRow,
+  TableCell,
+  TableHead,
+  TableBody,
+  TableContainer,
+  Paper,
 } from "@mui/material";
 import { IoMdAddCircle } from "react-icons/io";
-import { useNavigate } from "react-router-dom";
-import { AiOutlineHistory } from "react-icons/ai";
-import { useDispatch } from "react-redux";
+import { AiOutlineHistory, AiOutlineUser } from "react-icons/ai";
+import { useDispatch, useSelector } from "react-redux";
 import { motion } from "framer-motion";
 import { logOut } from "../../Redux/reducers/admin";
 import axios from "axios";
 
 const Navbar = ({ setOptionAdd }) => {
   const dispatch = useDispatch();
-  const navigate = useNavigate();
   const [history, setHistory] = useState(null);
+  const admin = useSelector((state) => state.admin.username);
+  const [openAdminHistory, setOpenAdminHistory] = useState(false);
+  const [openUserHistory, setOpenUserHistory] = useState(false);
+  const [userHis, setUserHis] = useState(null);
+  const userHistory = useRef();
+  const adminHistory = useRef();
   const handleAdd = () => {
     setOptionAdd(true);
   };
-  // const getHistory=async()={
-  //   const url=process.env.REACT_APP_SERVER+'/'
-  //     await axios.get()
-  // }
-  useEffect(() => {}, []);
+  const getHistory = async () => {
+    const url = process.env.REACT_APP_SERVER + "/admin/adminlog";
+    await axios
+      .post(url, { admin_name: admin })
+      .then((res) => {
+        console.log(res.data);
+        if (res.status === 200) setHistory(res.data);
+        else setHistory(null);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const handleOpenAdmin = () => {
+    setOpenAdminHistory((prev) => !prev);
+  };
+  //User History
+  const getUserHistory = async () => {
+    const url = process.env.REACT_APP_SERVER + "/admin/users/capturelog";
+    await axios
+      .get(url)
+      .then((res) => {
+        console.log(res.data);
+        if (res.status === 200) setUserHis(res.data);
+        else setUserHis(null);
+      })
+      .catch((err) => console.log(err));
+  };
+  const handleOpenUser = () => {
+    setOpenUserHistory((prev) => !prev);
+  };
+  useEffect(() => {
+    getHistory();
+    getUserHistory();
+  }, []);
+
   return (
     <>
-      {/* <OptionAdd optionAdd={optionAdd} /> */}
       <Box sx={{ px: 3 }}>
-        <Grid container spacing={2} justifyContent='center' alignItems='center'>
-          <Grid item xs={12} sm={5} md={7.5} lg={8}>
-            <Typography variant='h4'>Admin Page</Typography>
+        <Grid container spacing={2} justifyContent="center" alignItems="center">
+          <Grid item xs={12} sm={12} md={4} lg={4}>
+            <Typography variant="h4">Admin Page</Typography>
           </Grid>
-          <Grid item xs={4} sm={4} md={2} lg={2}>
+          <Grid item xs={6} sm={6} md={2} lg={2}>
+            <Button
+              ref={adminHistory}
+              variant="contained"
+              onClick={handleOpenAdmin}
+              color="primary"
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+              fullWidth
+              component={motion.div}
+              startIcon={<AiOutlineHistory />}
+            >
+              History
+            </Button>
             <Popover
+              open={openAdminHistory}
+              anchorEl={adminHistory.current}
+              onClose={handleOpenAdmin}
               anchorOrigin={{
                 vertical: "bottom",
                 horizontal: "right",
@@ -46,20 +100,119 @@ const Navbar = ({ setOptionAdd }) => {
                 vertical: "top",
                 horizontal: "right",
               }}
-            ></Popover>
+            >
+              {history === null ? (
+                <Typography variant="body1">
+                  The current user has not made any modifications
+                </Typography>
+              ) : (
+                <TableContainer component={Paper}>
+                  <Table sx={{ minWidth: 650 }} size="small">
+                    <TableHead>
+                      <TableRow>
+                        <TableCell>Change On</TableCell>
+                        <TableCell align="right">Change Type</TableCell>
+                        <TableCell align="right">Change Time</TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {history.map((item, index) => (
+                        <TableRow
+                          key={index}
+                          sx={{
+                            "&:last-child td, &:last-child th": { border: 0 },
+                          }}
+                        >
+                          <TableCell align="right">{item.change_on}</TableCell>
+                          <TableCell align="right">
+                            {item.change_type}
+                          </TableCell>
+                          <TableCell align="right">
+                            {item.change_time}
+                          </TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+              )}
+            </Popover>
           </Grid>
-
+          <Grid item xs={6} sm={6} md={2} lg={2}>
+            <Button
+              variant="contained"
+              ref={userHistory}
+              onClick={handleOpenUser}
+              color="primary"
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+              fullWidth
+              component={motion.div}
+              startIcon={<AiOutlineUser />}
+            >
+              User Mod
+            </Button>
+            <Popover
+              open={openUserHistory}
+              anchorEl={userHistory.current}
+              onClose={handleOpenUser}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "right",
+              }}
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+            >
+              {userHis === null || userHis.length === 0 ? (
+                <Typography variant="body1">
+                  No capture log has been enabled
+                </Typography>
+              ) : (
+                <TableContainer component={Paper}>
+                  <Table sx={{ minWidth: 650 }} size="small">
+                    <TableHead>
+                      <TableRow>
+                        <TableCell>Change On</TableCell>
+                        <TableCell align="right">Change Type</TableCell>
+                        <TableCell align="right">Change Time</TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {userHis.map((item, index) => (
+                        <TableRow
+                          key={index}
+                          sx={{
+                            "&:last-child td, &:last-child th": { border: 0 },
+                          }}
+                        >
+                          <TableCell align="right">{item.change_on}</TableCell>
+                          <TableCell align="right">
+                            {item.change_type}
+                          </TableCell>
+                          <TableCell align="right">
+                            {item.change_time}
+                          </TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+              )}
+            </Popover>
+          </Grid>
           <Grid
             item
             xs={6}
-            sm={4}
+            sm={6}
             md={2}
             lg={2}
             sx={{ display: "flex", justifyContent: "center" }}
           >
             <Button
-              variant='contained'
-              color='primary'
+              variant="contained"
+              color="primary"
               startIcon={<IoMdAddCircle />}
               component={motion.div}
               whileHover={{ scale: 1.1 }}
@@ -72,8 +225,8 @@ const Navbar = ({ setOptionAdd }) => {
           </Grid>
           <Grid item xs sx={{ display: "flex", justifyContent: "center" }}>
             <Button
-              variant='outlined'
-              color='primary'
+              variant="outlined"
+              color="primary"
               component={motion.div}
               whileHover={{ scale: 1.1 }}
               whileTap={{ scale: 0.9 }}
--- client/src/Components/MulitpleImages/MultipleImages.jsx
+++ client/src/Components/MulitpleImages/MultipleImages.jsx
@@ -1,4 +1,12 @@
-import { Button, Stack, Grid, Typography, Box ,CircularProgress} from "@mui/material";
+import {
+  Button,
+  Stack,
+  Grid,
+  Typography,
+  Box,
+  CircularProgress,
+  Avatar,
+} from "@mui/material";
 import { useState } from "react";
 import FileBase64 from "react-file-base64";
 import axios from "axios";
@@ -7,7 +15,8 @@ const MultipleImages = () => {
   const [file, setFile] = useState("");
   const [error, setError] = useState(null);
   const [result, setResult] = useState(null);
-  const [loading,setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [showImage, setShowImage] = useState(false);
   const getFile = (files) => {
     const newFile = [];
     if (files.length > 5) {
@@ -30,11 +39,13 @@ const MultipleImages = () => {
       await axios
         .post(url, { images: file })
         .then((res) => {
-          setResult(res.data);
+          setShowImage(true);
+          if (res.status === 200) setResult(res.data);
+          else setResult(null);
           console.log(res.data);
           setLoading(false);
         })
-        .catch((err) =>setError(err.response.data.msg));
+        .catch((err) => setError(err.response.data.msg));
       // setResult((prev) =>
       //   prev.map((p, index) => {
       //     return index === 3 ? [p[index][0], p[index][1].join(",")] : p[index];
@@ -48,72 +59,73 @@ const MultipleImages = () => {
   };
 
   return (
-    <Stack mt={2} spacing={3} justifyContent='center'>
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <FileBase64
-          value={file}
-          multiple={true}
-          onDone={(file) => {
-            getFile(file);
-          }}
-        />
-      </div>
-      <Grid container justifyContent='center'>
-        {file!=="" &&
-          file.map((f, i) => {
-            return (
-              <img
-                key={i}
-                src={f}
-                style={{ maxWidth: "450px", margin: "20px",objectFit:'cover' }}
+    <>
+      <Grid container>
+        <Grid item xs={showImage ? 7 : 12}>
+          <Stack mt={2} spacing={3} justifyContent="center">
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              <FileBase64
+                value={file}
+                multiple={true}
+                onDone={(file) => {
+                  getFile(file);
+                }}
               />
-            );
-          })}
+            </div>
+            <Grid container justifyContent="center">
+              {file !== "" &&
+                file.map((f, i) => {
+                  return (
+                    <img
+                      key={i}
+                      src={f}
+                      style={{
+                        maxWidth: "450px",
+                        margin: "20px",
+                        objectFit: "cover",
+                      }}
+                      alt="uploaded"
+                    />
+                  );
+                })}
+            </Grid>
+            {error && (
+              <Typography variant="h5" color="error" align="center">
+                {error}
+              </Typography>
+            )}
+            <Box sx={{ display: "flex", justifyContent: "center" }}>
+              {loading && <CircularProgress />}
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={handleUpload}
+              >
+                {file !== "" ? "Reset" : "Upload"}
+              </Button>
+            </Box>
+          </Stack>
+        </Grid>
+        <Grid item xs={showImage ? 4 : 0}>
+          <Stack spacing={3} justifyContent="center">
+            {result !== null &&
+              result.recognizedPeople.map((p, i) => {
+                return (
+                  <Box key={i}>
+                    <Avatar
+                      src={require(`../../../../user_images/uploads/${p.img}`)}
+                      alt={p.name}
+                    />
+                    <Typography key={i} variant="h5" align="center">
+                      {p.name}
+                    </Typography>
+                  </Box>
+                );
+              })}
+          </Stack>
+        </Grid>
       </Grid>
-      {error && (
-        <Typography variant='h5' color='error' align='center'>
-          {error}
-        </Typography>
-      )}
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-        }}
-      >
-        <Stack spacing={2}>
-          {result &&<>
-              <Typography  variant='h5' align='center'>
-                {"Number of images uploaded"} :{result.uploaded}
-              </Typography>
-              <Typography  variant='h5' align='center'>
-                {"Number of People Detected"} :{result.detected} 
-              </Typography>
-              <Typography  variant='h5' align='center'>
-                {"Number of People Recognized"} :{result.recognized}
-              </Typography>
-              <Typography  variant='h5' align='center'>
-                {"Names of People Found"} :{result.recognizedNames.length>0?result.recognizedNames.join(' , '):"No People Found"}
-              </Typography>
-              </>
-            }
-          <Box mx={'auto'}>
-          {loading && <CircularProgress />}
-          </Box>
-          <Box mx={"auto !important"}>
-            <Button
-              size='large'
-              variant='contained'
-              color='primary'
-              onClick={handleUpload}
-            >
-              {result === null ? "Uploads" : "Upload Different Image"}
-            </Button>
-          </Box>
-        </Stack>
-      </div>
-    </Stack>
+    </>
   );
 };
 
