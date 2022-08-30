767537940e262137fb0d3a4c26887796d8c3d4a8
Vedansh Agarwal <101228748+vedansh-agarwal@users.noreply.github.com>
2022-08-30 15:00:07 +0530

    Update checkUserAuth.js

--- server/utils/checkUserAuth.js
+++ server/utils/checkUserAuth.js
@@ -5,9 +5,9 @@ const checkUserAuth = async (req, res, next) => {
     if(!authorization) {
         return res.status(401).json({msg: "No authorization header"});
     }
-    const token = authorization.split(" ")[1];
+//     const token = authorization.split(" ")[1];
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        const decoded = jwt.verify(authorization, process.env.JWT_SECRET_KEY);
         req.body.last_modified_by = decoded.name;
         next();
     } catch (err) {
