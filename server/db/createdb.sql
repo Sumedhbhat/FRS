509d44116e58f0589a4f8d29165ac2abce1d6fad
SumedhBhat <sumedhbhat01@gmail.com>
2022-08-31 18:15:38 +0530

    Changes database

--- server/db/createdb.sql
+++ server/db/createdb.sql
@@ -5,9 +5,11 @@ USE `FRS`;
 
 /* Creating admin table */
 CREATE TABLE `FRS`.`admin` (
-  `username` VARCHAR(20) NOT NULL,
-  `password` VARCHAR(20) NOT NULL,
-  PRIMARY KEY (`username`));
+  `name` VARCHAR(20) NOT NULL,
+  `email` VARCHAR(200) NOT NULL,
+  `password` VARCHAR(100) NOT NULL,
+  `status` VARCHAR(8) DEFAULT 'disabled',
+  PRIMARY KEY (`email`));
   
 /* Creating admin logs table */
 CREATE TABLE `FRS`.`admin_log` (
@@ -62,7 +64,7 @@ CREATE TABLE `FRS`.`user_capture_log` (
 
 /* Trigger for creating log on insert user */
 DELIMITER $$
-CREATE DEFINER=`vedanta`@`localhost` TRIGGER `create_log_on_insert` AFTER INSERT ON `user` FOR EACH ROW BEGIN
+CREATE TRIGGER `create_log_on_insert` AFTER INSERT ON `user` FOR EACH ROW BEGIN
 	INSERT INTO `admin_log` (`change_by`, `change_on`, `change_type`)
     VALUE (NEW.`last_modified_by`, NEW.`user_id`, "INSERT");
     INSERT INTO `user_change_log` (`change_by`, `change_type`, `user_id`, `base_img`, `img_ext`, `name`, `mob_no`, `gender`, `city`, `department`, `date_created`) 
@@ -72,7 +74,7 @@ DELIMITER ;
 
 /* Trigger for creating log on update user */
 DELIMITER $$
-CREATE DEFINER=`vedanta`@`localhost` TRIGGER `create_log_on_update` AFTER UPDATE ON `user` FOR EACH ROW BEGIN
+CREATE TRIGGER `create_log_on_update` AFTER UPDATE ON `user` FOR EACH ROW BEGIN
     IF (NEW.`base_img` != OLD.`base_img` OR NEW.`name` != OLD.`name` OR NEW.`mob_no` != OLD.`mob_no` OR NEW.`gender` != OLD.`gender` OR NEW.`city` != OLD.`city` OR NEW.`department` != OLD.`department`) THEN
         INSERT INTO `admin_log` (`change_by`, `change_on`, `change_type`)
 		VALUE (NEW.`last_modified_by`, NEW.`user_id`, "UPDATE");
@@ -84,7 +86,7 @@ DELIMITER ;
 
 /* Trigger for creating log on delete user */
 DELIMITER $$
-CREATE DEFINER = CURRENT_USER TRIGGER `FRS`.`create_log_on_delete` AFTER DELETE ON `user` FOR EACH ROW
+CREATE TRIGGER `FRS`.`create_log_on_delete` AFTER DELETE ON `user` FOR EACH ROW
 BEGIN
 	INSERT INTO `admin_log` (`change_by`, `change_on`, `change_type`)
 	VALUE (OLD.`last_modified_by`, OLD.`user_id`, "DELETE");
@@ -95,9 +97,6 @@ DELIMITER ;
 
 /* Get user view */
 CREATE 
-    ALGORITHM = UNDEFINED 
-    DEFINER = `vedanta`@`localhost` 
-    SQL SECURITY DEFINER
 VIEW `FRS`.`get_user` AS
     SELECT 
 		`FRS`.`user`.`user_id` AS `user_id`,
@@ -113,7 +112,7 @@ VIEW `FRS`.`get_user` AS
 
 /* Delete user procedure */
 DELIMITER $$
-CREATE DEFINER=`vedanta`@`localhost` PROCEDURE `delete_user`(IN usr_id VARCHAR(36), IN last_modifier VARCHAR(20))
+CREATE PROCEDURE `delete_user`(IN usr_id VARCHAR(36), IN last_modifier VARCHAR(20))
 BEGIN
 	UPDATE `user` SET `last_modified_by` = last_modifier WHERE `user_id` = usr_id;
 	SELECT `base_img` FROM `user` WHERE `user_id` = usr_id;
@@ -138,6 +137,28 @@ BEGIN
 END$$
 DELIMITER ;
 
-/* Inserting dummy admin values */
-INSERT INTO `FRS`.`admin` (`username`, `password`) VALUES ('vedansh', 'password');
-INSERT INTO `FRS`.`admin` (`username`, `password`) VALUES ('sumedh', 'sudu_1000');
+/* Get user capture log procedure */
+DELIMITER $$
+CREATE PROCEDURE `get_capture_log` ()
+BEGIN
+  SELECT uc.`user_id`, ut.`name`, uc.`in/out`, uc.`date_time`
+  FROM `user_capture_log` uc
+  JOIN `user` ut
+  ON ut.`user_id` = uc.`user_id`
+  WHERE uc.`recognition_status` = "TRUE"
+  ORDER BY `date_time` DESC
+  LIMIT 20;
+END$$
+DELIMITER ;
+
+/* Get admin log procedure */
+DELIMITER $$
+CREATE PROCEDURE `get_admin_log` (IN admin_name VARCHAR(20))
+BEGIN
+	SELECT `change_on`, `change_type`, `change_time`
+	FROM `admin_log`
+	WHERE `change_by` = admin_name
+	ORDER BY `change_time` DESC
+	LIMIT 20;
+END$$
+DELIMITER ;
--- server/db/dbconnect.js
+++ server/db/dbconnect.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2");
-const {user, host, password, database} = require("./db_config");
+const { user, host, password, database } = require("./db_config");
 
 const db = mysql.createConnection({
   user: user,
   host: host,
-  password: password,  /* "password" "sudu_1000" */
+  password: password /* "password" "sudu_1000" */,
   database: database,
 });
 
