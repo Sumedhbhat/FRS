0204c95a97288ec187450d5b318cff02ccf5dffa
SumedhBhat <sumedhbhat01@gmail.com>
2022-08-31 18:13:00 +0530

    Added packages in package.json

--- server/db/dbconnect.js
+++ server/db/dbconnect.js
@@ -2,10 +2,10 @@ const mysql = require("mysql2");
 const {user, host, password, database} = require("./db_config");
 
 const db = mysql.createConnection({
-  user: user,
-  host: host,
-  password: password,  /* "password" "sudu_1000" */
-  database: database,
+  user: "vedanta",
+  host: "localhost",
+  password: "vedu123",  /* "password" "sudu_1000" */
+  database: "FRS",
 });
 
 module.exports = db;
